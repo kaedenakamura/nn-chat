7ea1b4555d3e2555d39bc1900cb9609be944b0ca
kaedenamakura <pvqv315@yahoo.co.jp>
2025-08-17 20:52:05 +0900

    削除機能の追加

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const http = require('node:http');
 const auth = require("http-auth");
 const router = require("./lib/router.js");
 
+// Basic認証設定
 const basic = auth.basic({
-  realm: "Enter username and password" ,
+  realm: "Enter username and password",
   file: "./users.htpasswd" // ユーザー名とパスワードのファイル
 });
+
 const server = http.createServer(basic.check((req, res) => {
-    router.route(req, res);
-  }))
-  .on('error', e => {
-    console.error('Server Error', e);
-  })
-  .on('clientError', e => {
-    console.error('Client Error', e);
-  });
+  // 認証成功時のユーザー名を req.user にセット
+  req.user =  req.user || "anonymous"; // ←ここで req.user を posts-handler に渡す
+  router.route(req, res);
+}))
+.on('error', e => {
+  console.error('Server Error', e);
+})
+.on('clientError', e => {
+  console.error('Client Error', e);
+});
 
 const port = 8000;
 server.listen(port, () => {
